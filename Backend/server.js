c2206d7290d8adafb1be2c5e67f1a52611da2cb7
ANISH4623 <anishpbhagat@gmail.com>
2025-03-10 17:54:47 -0600

    Added cors Functionality

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const { initializeDatabases } = require("./config/db");
 const apiRoutes = require("./routes/api");
-
+const cors = require("cors");
 const app = express();
 
 // Middleware
@@ -13,10 +13,14 @@ app.use(express.json());
 // Initialize databases
 initializeDatabases();
 
+const corOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corOptions));
 // Routes
 app.use("/api", apiRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 // Start server
 const PORT = 8080;
 const HOST = "localhost";
